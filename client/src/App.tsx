27fa6fe4f1e07a4282fed342f118a49490ae26b3
thortineoc <angkubicka99@gmail.com>
2021-09-16 20:05:20 +0200

    Adding routes guard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import MainRouter from "./shared/MainRouter";
+import MainRouter from "./Routers/MainRouter";
 import Navbar from "./shared/Navbar/Navbar";
 
 function App() {
--- client/src/Routers/MainRouter.tsx
+++ client/src/Routers/MainRouter.tsx
@@ -1,12 +1,17 @@
 import React, { ReactElement } from "react";
+import { useSelector } from "react-redux";
 import { Switch, Route } from "react-router-dom";
 import CoursesDisplay from "../pages/CoursesDisplay/CoursesDisplay";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Homepage from "../pages/Homepage/Homepage";
 import Login from "../pages/Login/Login";
 import Registration from "../pages/Registration/Registration";
+import GuardedRoute from "../shared/GuardedRoute/GuardedRoute";
+import { selectUser } from "../slices/UserSlice";
 
 function MainRouter(): ReactElement {
+  const user = useSelector(selectUser);
+  console.log(user);
   return (
     <Switch>
       <Route exact path="/login">
@@ -15,9 +20,7 @@ function MainRouter(): ReactElement {
       <Route exact path="/register">
         <Registration />
       </Route>
-      <Route exact path="/courses">
-        <CoursesDisplay />
-      </Route>
+      <GuardedRoute component={CoursesDisplay} path="/courses" auth={user} />
       <Route exact path="/">
         <Homepage />
       </Route>
