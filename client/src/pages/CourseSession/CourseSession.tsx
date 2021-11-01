9ad9e16b9a1bfd3564a105bd6476d9a32cef475c
thortineoc <angkubicka99@gmail.com>
2021-11-01 14:35:12 +0100

    Get random words

--- client/src/pages/CourseSession/CourseSession.tsx
+++ client/src/pages/CourseSession/CourseSession.tsx
@@ -23,14 +23,17 @@ interface allTranslationsType {
 function CourseSession(): ReactElement {
   const user = useSelector(selectUser);
   const url = `https://localhost:5001/api/course/2/category/2`;
+  const urlToGetRandomWords = `https://localhost:5001/api/random/2`;
+
   const [allTranslations, setAllTranslations] = useState<
     allTranslationsType[] | undefined
   >([]);
   const [sessionTranslations, setSessionTranslations] = useState<
     allTranslationsType[] | undefined
   >([]);
+  const [randomWords, setRandomWords] = useState<string[] | undefined>([]);
 
-  const numberOfWordsInSession = 3;
+  const numberOfWordsInSession = 4;
 
   useEffect(() => {
     axios
@@ -42,6 +45,16 @@ function CourseSession(): ReactElement {
         setAllTranslations(
           res.data[0].userCourses[0].course.categories[0].translations
         );
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(urlToGetRandomWords, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      })
+      .then((res) => {
+        setRandomWords(res.data);
+        console.log(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
