33ee0e981c3a8bb56c71e0dbdba9a40c3a64aafe
thortineoc <angkubicka99@gmail.com>
2021-11-09 13:53:49 +0100

    sessionTranslations not updating

--- client/src/pages/CourseSession/CourseSession.tsx
+++ client/src/pages/CourseSession/CourseSession.tsx
@@ -6,6 +6,7 @@ import { selectSession } from "../../slices/SessionSlice";
 import { getRandomInt, shuffle } from "../../helpers/getRandomHelper";
 import "./CourseSession.scss";
 import { Link } from "react-router-dom";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
 
 interface translationUserProgressType {
   appUserId: number;
@@ -49,12 +50,13 @@ function CourseSession(): ReactElement {
   const [points, setPoints] = useState(0);
   const [count, setCount] = useState(1);
 
-  const wordsPerRound = 5;
+  const wordsPerRound = 20;
   const pointsForGoodAnswer = 10;
-  const numberOfWordsInSession = 4;
+  const numberOfDifferentWordsInSession = 2;
+  const repeatUntilLearned = 3;
 
   const setUpSessionStep = () => {
-    let num = getRandomInt(numberOfWordsInSession);
+    let num = getRandomInt(numberOfDifferentWordsInSession);
     if (sessionTranslations && sessionTranslations[num]) {
       setCurrentTranslation(sessionTranslations[num]);
       if (randomWords?.includes(sessionTranslations[num].wordTo)) {
@@ -75,7 +77,14 @@ function CourseSession(): ReactElement {
   const [borderStyle, setBorderStyle] = useState("session-translation-card");
   const [clicked, setClicked] = useState("");
 
+  useEffect(() => {
+    console.log("sasas");
+    console.log(sessionTranslations);
+  }, [sessionTranslations]);
+
   const checkAnswer = (word: string) => {
+    let newSessionArray: allTranslationsType[] | undefined = [];
+
     if (count === wordsPerRound) {
       setTimeout(() => {
         setEnd(true);
@@ -86,12 +95,58 @@ function CourseSession(): ReactElement {
     if (word === currentTranslation?.wordTo) {
       setStyles("session-translation-card correct");
       setPoints(points + pointsForGoodAnswer);
+
+      const newTranslation = { ...currentTranslation };
+      newTranslation.translationUserProgress[0].timesRepeated =
+        newTranslation.translationUserProgress[0].timesRepeated + 1;
+      setCurrentTranslation(newTranslation);
+      while (
+        currentTranslation.translationUserProgress[0].timesRepeated !==
+        newTranslation.translationUserProgress[0].timesRepeated
+      ) {}
+      if (
+        currentTranslation.translationUserProgress[0].timesRepeated ===
+        repeatUntilLearned
+      ) {
+        // zaznacz jako nauczone, dodaj punkty, post z iloscia repeated
+        const nextWordIndex = allTranslations?.findIndex(
+          (word: allTranslationsType) =>
+            word.translationUserProgress[0].timesRepeated !==
+              repeatUntilLearned && !sessionTranslations?.includes(word)
+        );
+        if (nextWordIndex === undefined) {
+          setEnd(true);
+        } else {
+          if (allTranslations && nextWordIndex) {
+            const nextWord = allTranslations[nextWordIndex];
+            newSessionArray = sessionTranslations?.filter(
+              (el) => el !== currentTranslation
+            );
+            if (newSessionArray && newSessionArray.length) {
+              newSessionArray.push(nextWord);
+              console.log(newSessionArray);
+              setSessionTranslations([...newSessionArray]);
+            }
+          }
+        }
+      }
     } else {
       setStyles("session-translation-card wrong");
       setBorderStyle("session-translation-card was-correct");
     }
     setOverlay(true);
+    if (newSessionArray?.length !== 0) {
+      console.log(newSessionArray);
+      console.log(sessionTranslations);
+      //while (sessionTranslations !== newSessionArray) {}
+    }
     setTimeout(() => {
+      if (newSessionArray?.length !== 0) {
+        console.log(sessionTranslations);
+        console.log(newSessionArray);
+        console.log(sessionTranslations === newSessionArray);
+        // while (sessionTranslations !== newSessionArray) {}
+      }
       setUpSessionStep();
       setStyles("session-translation-card");
       setBorderStyle("session-translation-card");
@@ -118,9 +173,6 @@ function CourseSession(): ReactElement {
           headers: { Authorization: `Bearer ${user.token}` },
         })
         .then((res) => {
-          console.log(
-            res.data[0].userCourses[0].course.categories[0].translations
-          );
           setAllTranslations(
             res.data[0].userCourses[0].course.categories[0].translations
           );
@@ -141,7 +193,7 @@ function CourseSession(): ReactElement {
   useEffect(() => {
     const arr = [];
     for (let i = 0; i < allTranslations!.length; i++) {
-      if (arr.length === numberOfWordsInSession) {
+      if (arr.length === numberOfDifferentWordsInSession) {
         break;
       }
       if (
@@ -164,7 +216,7 @@ function CourseSession(): ReactElement {
             START
           </div>
           <img
-            src="/assets/images/startSession.jpg"
+            src="assets/images/startSession.jpg"
             alt="start"
             className="start-img"
           />
@@ -191,6 +243,15 @@ function CourseSession(): ReactElement {
             <div>{points + " points"}</div>
           </div>
           <div className="session-word">{currentTranslation.wordFrom}</div>
+          <div className="light-bulb-icons">
+            {[
+              ...Array(
+                currentTranslation.translationUserProgress[0].timesRepeated
+              ),
+            ].map(() => (
+              <EmojiObjectsIcon color="primary" />
+            ))}
+          </div>
           <div className="session-translation-group">
             {randomWords.map((word, i) => (
               <div
